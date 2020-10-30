88decaf0fd40a0955ef94cec35419ff62ba1568d
violetfreesia <violetfreesia@foxmail.com>
2020-10-31 00:24:14 +0800

    增加当前窗口状态的全局

--- src/static/constants.ts
+++ src/static/constants.ts
@@ -1,5 +1,14 @@
-// 常量存放地址
+// 常量存放
 export const DEFAULT_POST_PAGE_SIZE = 10
 export const DEFAULT_ARCHIVES_PATH = 'archives'
-export const side = {}
+
+export enum WindowName {
+    Home,       //首页
+    PostEditor, //文章编辑界面
+    Article,    //首页文章界面
+    Menu,       //首页菜单界面
+    Tag,        //首页标签界面
+    Category,   //首页分类界面
+    Setting     //首页设置界面
+}
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,12 @@
 // 系统配置
 const systemConfig = Symbol()
+// 当前app窗口界面
+const currentAppWindow = Symbol()
+// 当前非文章编辑页的窗口界面
+const currentHomeWindow = Symbol()
 // 界面全局变量登记
 export default {
-    systemConfig
+    systemConfig,
+    currentAppWindow,
+    currentHomeWindow
 }
