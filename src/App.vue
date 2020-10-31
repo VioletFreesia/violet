4ac143116dfc0e86c4637f30a3988f263a8b3023
violetfreesia <violetfreesia@foxmail.com>
2020-10-31 11:49:31 +0800

    设置全局滚动条样式

--- src/App.vue
+++ src/App.vue
@@ -28,4 +28,22 @@ html, body {
   width: 100%;
   height: 100%;
 }
+
+/*控制整个滚动条*/
+::-webkit-scrollbar {
+  background-color: rgba(255,255,255,0.8);
+  width: 6px;
+
+}
+
+/*滚动条两端方向按钮*/
+::-webkit-scrollbar-button {
+  display: none;
+}
+
+/*滚动条中间滑动部分*/
+::-webkit-scrollbar-thumb {
+  background-color: violet;
+  border-radius: 3px;
+}
 </style>
