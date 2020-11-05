5bee781b6a7167ac12f6d97661f06b9c100688e7
violetfreesia <violetfreesia@foxmail.com>
2020-11-06 02:50:58 +0800

    postInfo增加isSelected属性

--- src/interfaces/public/post.ts
+++ src/interfaces/public/post.ts
@@ -7,10 +7,11 @@ export interface PostInfo {
     img: string         //文章封面
     tags: string[]      //文章标签
     category: Category  //文章分类
-    modifyDate: string    //文章最后一次修改时间
+    modifyDate: string  //文章最后一次修改时间
     isDeploy: boolean   //文章是否发布
     isTop: boolean      //文章是否置顶
     isHide: boolean     //文章是否隐藏
+    isSelected: boolean //文章是否被选中
     fileName: string    //文章的本地文件名
 }
 
