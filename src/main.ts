aae91ec94568b72e1bdec136185cafcbe119d01e
violetfreesia <violetfreesia@foxmail.com>
2020-11-21 19:51:03 +0800

    引入第三方动画库

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import App from './App.vue'
 import 'ant-design-vue/dist/antd.css'
 import '@/assets/styles/iconfont.css'
 import '@/assets/styles/main.css'
+import 'animate.css/animate.min.css'
 
 createApp(App).use(Antd).mount('#app')
