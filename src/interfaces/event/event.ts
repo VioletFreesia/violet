5c11d83af1eec3cdae609b7abb9b351bf8ac7b17
violetfreesia <violetfreesia@foxmail.com>
2020-11-21 20:52:01 +0800

    添加事件接口

---
+++ src/interfaces/event/event.ts
@@ -0,0 +1,13 @@
+export default interface Event {
+}
+
+
+export interface PostInfoEvent {
+    Top: string
+    UnTop: string
+    Hide: string
+    UnHide: string
+    Deploy: string
+    UnDeploy: string
+    Modify: string
+}
