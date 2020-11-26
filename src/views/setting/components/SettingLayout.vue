bfdfcca7ba2d5dd51b29250a74645f497bd898d2
violetfreesia <violetfreesia@foxmail.com>
2020-11-26 13:35:17 +0800

    添加设置页面布局

---
+++ src/views/setting/components/SettingLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="setting-layout">
+    <a-row type="flex" class="content">
+      <a-col class="content" :span="4">
+        <div class="side">
+          <slot name="side"></slot>
+        </div>
+      </a-col>
+      <a-col class="content" :span="20">
+        <div class="setting-items">
+          <slot></slot>
+        </div>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SettingLayout"
+}
+</script>
+
+<style scoped>
+.side,
+.content,
+.setting-items,
+#setting-layout {
+  height: 100%;
+  overflow: hidden;
+}
+</style>
