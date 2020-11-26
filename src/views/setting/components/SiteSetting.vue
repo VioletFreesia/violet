5af3ca21224865e755a2905aca6aaa66b8833d30
violetfreesia <violetfreesia@foxmail.com>
2020-11-26 13:37:17 +0800

    测试滚动锚点(测试中)

--- src/views/setting/components/SiteSetting.vue
+++ src/views/setting/components/SiteSetting.vue
@@ -1,26 +1,61 @@
 <template>
   <div id="site-setting">
-    <a-row type="flex">
-      <a-col :span="6">
-       <div class="side">
-          网站配置
-       </div>
-      </a-col>
-      <a-col :span="18"></a-col>
-    </a-row>
+    <setting-layout>
+      <template #side>
+        <div class="side">
+          <a-anchor wrapperClass="test"
+                    :get-container="test">
+            <a-anchor-link href="#test01" title="Basic demo"/>
+            <a-anchor-link href="#test02" title="Static demo"/>
+            <a-anchor-link href="#test03" title="API"></a-anchor-link>
+          </a-anchor>
+        </div>
+      </template>
+      <template #default>
+        <div class="site-setting-items">
+          <div id="test01" class="box">test01</div>
+          <div id="test02" class="box">test02</div>
+          <div id="test03" class="box">test03</div>
+          <div id="test04" class="box">test04</div>
+        </div>
+      </template>
+    </setting-layout>
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue'
+import SettingLayout from '@/views/setting/components/SettingLayout.vue'
 
 export default defineComponent({
-  name: "SiteSetting"
+  name: "SiteSetting",
+  components: {SettingLayout},
+  methods: {
+    test() {
+      return document.querySelector('#site-setting-items')
+    }
+  }
 })
 </script>
 
 <style scoped>
+#site-setting {
+  height: 100%;
+}
+
 .side {
-  padding-left: 10px;
+  height: 100%;
+  background-color: violet;
+}
+
+.site-setting-items {
+  height: 100%;
+  overflow: scroll;
+}
+
+.box {
+  height: 300px;
+  margin: 10px;
+  background-color: pink;
 }
 </style>
