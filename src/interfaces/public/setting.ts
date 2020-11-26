811a9aaa90d1e04fbb924f67b9c75ecdfef27619
violetfreesia <violetfreesia@foxmail.com>
2020-11-26 19:28:11 +0800

    修改网站配置接口

--- src/interfaces/public/setting.ts
+++ src/interfaces/public/setting.ts
@@ -4,23 +4,17 @@ export interface ThemeConfig {
 
 // 网站配置
 export interface SiteConfig {
-    themeName: string
-    postPageSize: number
-    archivesPageSize: number
-    siteName: string
-    siteDescription: string
-    footerInfo: string
-    showFeatureImage: boolean
-    postUrlFormat: string
-    tagUrlFormat: string
-    dateFormat: string
-    feedFullText: boolean
-    feedCount: number
-    faviconPath: string
-    avatarPath: string
-    archivesPath: string
-    postPath: string
-    tagPath: string
+    themeName: string           //主题名字
+    postPageSize: number        //首页单页文章数量  默认10
+    archivesPageSize: number    //归档页单页文章数量 默认10
+    siteName: string            //网站名字  默认violet
+    siteDescription: string     //网站描述  默认 "好记性不如烂'笔头'"
+    faviconPath: string         //logo路径 不能更改 /favicon.png
+    avatarPath: string          //头像路径  不能更改 /avatar.png
+    archivesPath: string        //归档路径 默认/archive
+    postPath: string            //文章路径 默认/posts
+    tagPath: string             //标签路径 默认/tags
+    categoryPath: string        //分类路径 默认/categories
 }
 
 // 远程配置
