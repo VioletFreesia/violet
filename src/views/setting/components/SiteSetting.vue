fcf236d51331656b986f258644759b9bba6bc989
violetfreesia <violetfreesia@foxmail.com>
2020-11-26 15:12:12 +0800

    完成锚点定位功能

--- src/views/setting/components/SiteSetting.vue
+++ src/views/setting/components/SiteSetting.vue
@@ -4,7 +4,9 @@
       <template #side>
         <div class="side">
           <a-anchor wrapperClass="test"
-                    :get-container="test">
+                    :get-container="getContainer"
+                    :get-current-anchor="()=>'#test01'"
+                    @click="preventDefault">
             <a-anchor-link href="#test01" title="Basic demo"/>
             <a-anchor-link href="#test02" title="Static demo"/>
             <a-anchor-link href="#test03" title="API"></a-anchor-link>
@@ -12,11 +14,15 @@
         </div>
       </template>
       <template #default>
-        <div class="site-setting-items">
-          <div id="test01" class="box">test01</div>
-          <div id="test02" class="box">test02</div>
-          <div id="test03" class="box">test03</div>
-          <div id="test04" class="box">test04</div>
+        <div id="site-setting-items">
+          <a-form :layout="'vertical'" >
+            <a-form-item label="Field A">
+              <a-input placeholder="input placeholder" />
+            </a-form-item>
+            <a-form-item label="Field B">
+              <a-input  placeholder="input placeholder" />
+            </a-form-item>
+          </a-form>
         </div>
       </template>
     </setting-layout>
@@ -30,9 +36,12 @@ import SettingLayout from '@/views/setting/components/SettingLayout.vue'
 export default defineComponent({
   name: "SiteSetting",
   components: {SettingLayout},
-  methods: {
-    test() {
-      return document.querySelector('#site-setting-items')
+  setup() {
+    let getContainer = () => document.querySelector('#site-setting-items')
+    let preventDefault = (e: Event) => e.preventDefault()
+    return {
+      getContainer,
+      preventDefault
     }
   }
 })
@@ -44,18 +53,13 @@ export default defineComponent({
 }
 
 .side {
+  padding-left: 3px;
   height: 100%;
-  background-color: violet;
 }
 
-.site-setting-items {
+#site-setting-items {
   height: 100%;
   overflow: scroll;
-}
-
-.box {
-  height: 300px;
-  margin: 10px;
-  background-color: pink;
+  padding-bottom: 100px;
 }
 </style>
