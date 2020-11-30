56034c0f203ec0461e4e571bcd5ffc0034e040fb
violetfreesia <violetfreesia@foxmail.com>
2020-11-30 18:38:37 +0800

    增加获取文章所有文章信息函数，完善注释

--- src/server/violet-app.ts
+++ src/server/violet-app.ts
@@ -1,38 +1,52 @@
 import {SystemConfig} from "@/interfaces/public/setting"
 import eventHandlerRegister from "@/server/event-handler/event-handler"
 import {PostInfo} from "@/interfaces/public/post"
-// import path from 'path'
+import JsonDB from "@/tools/jsondb"
+import path from 'path'
 
 export class VioletApp {
     private _systemConfig: SystemConfig     //系统配置
-    // private _postInfoDB: JsonDB | undefined   //文章基本信息数据库
+    private _postInfoDB: JsonDB | undefined   //文章基本信息数据库
 
     constructor(systemConfig: SystemConfig) {
         this._systemConfig = systemConfig
         // 注册事件
         eventHandlerRegister(this)
-        console.log("violetApp已启动")
+        console.log("violetApp已启动", this._systemConfig)
     }
 
+    /**
+     * 设置系统配置
+     * @param value
+     */
     set systemConfig(value: SystemConfig) {
         this._systemConfig = value
     }
 
+    /**
+     * 获取系统配置
+     */
     get systemConfig(): SystemConfig {
         return this._systemConfig
     }
 
-    // get postInfoDB(): JsonDB | undefined {
-    //     // 仅在第一次访问时创建
-    //     if (!this._postInfoDB) {
-    //         let postInfoPath = path.join(this._systemConfig.workplace,
-    //             '.violet/post-info.json')
-    //         this._postInfoDB = new JsonDB(postInfoPath)
-    //     }
-    //     return this._postInfoDB
-    // }
+    /**
+     * 获取文章信息相关数据库
+     */
+    get postInfoDB(): JsonDB {
+        // 仅在第一次访问时创建
+        if (!this._postInfoDB) {
+            let postInfoPath = path.join(this._systemConfig.workplace,
+                '.violet/post-info.json')
+            this._postInfoDB = new JsonDB(postInfoPath)
+        }
+        return this._postInfoDB
+    }
 
-    get postInfo(): PostInfo[] {
-        return []
+    /**
+     * 获取所有文章信息
+     */
+    postInfo(): PostInfo[] {
+        return this.postInfoDB.row().postInfos
     }
 }
