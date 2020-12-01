fb9efd52560bf59599181bd4cbb42654d206967d
violetfreesia <violetfreesia@foxmail.com>
2020-12-01 17:14:47 +0800

    增加操作枚举

--- src/instance/enum/enums.ts
+++ src/instance/enum/enums.ts
@@ -10,17 +10,19 @@ export enum WindowName {
     Setting     //首页设置界面
 }
 
-// 文章卡片触发的操作名
+// 文章首页触发的操作名
 export enum PostCardOperationType {
-    UnDeploy,          //取消发布
-    DeployArticle,        //发布文章
+    UnDeploy,           //取消发布
+    DeployArticle,      //发布文章
     UnHide,             //取消隐藏
     HideArticle,        //隐藏文章
     TopArticle,         //置顶文章
-    UnTop,             //取消置顶
-    DeleteArticle,      //删除文章
+    UnTop,              //取消置顶
+    DeleteArticle,      //删除文章（移入回收站）
+    UnDelete,           //删除文章（移除回收站）
     ModifyProperties,   //修改属性
     EditPost,           //编辑/查看文章
-    BatchOption,        //批量编辑
-    QuitBatchOption    //退出批量编辑
+    BatchOption,       //批量编辑
+    QuitBatchOption,   //退出批量编辑
+    GetAllPostInfos    //获取所有文章
 }
