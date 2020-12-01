6aa8946dd1d79af53d43741e19cb6985dbcc848f
violetfreesia <violetfreesia@foxmail.com>
2020-12-01 22:24:12 +0800

    增加加载获取编辑器初始化属性

--- src/views/article/Article.vue
+++ src/views/article/Article.vue
@@ -43,6 +43,10 @@ export default defineComponent({
     let currentAppWindow = inject<Ref<WindowName>>(store.currentAppWindow)!
     // 获取所有文章信息
     let postInfos = inject<Ref<PostInfo[]>>(store.article.postInfos)!
+    // 编辑器模式
+    let isEdit = inject<Ref<boolean>>(store.article.isEdit)!
+    // 加载的文件名
+    let filename = inject<Ref<string>>(store.article.filename)!
     // 是否显示属性编辑弹窗
     let showModifyModel = ref<boolean>(false)
     // 刷新文章信息
@@ -50,62 +54,65 @@ export default defineComponent({
       emit('refresh')
     }
     // 文章事件的控制器
-    let postCardOperationHandler = (operationType: PostCardOperationType, postId: string) => {
-      switch (operationType) {
-        case PostCardOperationType.EditPost:
-          currentAppWindow.value = WindowName.PostEditor
-          break;
-        case PostCardOperationType.ModifyProperties:
-          showModifyModel.value = true
-          break
-        case PostCardOperationType.BatchOption:
-          // 进入批量编辑前让所有文章处于未选中状态
-          postInfos.value.forEach(postInfo => {
-            if (postInfo.isSelected) {
-              postInfo.isSelected = false
-            }
-          })
-          break
-        default:
-          api.postApi.postInfoHandle(operationType)!([postId]).then(success => {
-            if (success) {
-              let postInfo = postInfos.value.filter(item => item.id === postId)[0]
-              switch (operationType) {
-                case PostCardOperationType.DeleteArticle:
-                  postInfo.isDeleted = true
-                  message.success('文章已放入回收站')
-                  break
-                case PostCardOperationType.UnTop:
-                  postInfo.isTop = false
-                  message.success('已取消置顶')
-                  break
-                case PostCardOperationType.TopArticle:
-                  postInfo.isTop = true
-                  postInfo.isHide = false
-                  message.success('文章已置顶')
-                  break
-                case PostCardOperationType.UnHide:
-                  postInfo.isHide = false
-                  message.success('已取消隐藏')
-                  break
-                case PostCardOperationType.HideArticle:
-                  postInfo.isHide = true
-                  postInfo.isTop = false
-                  message.success('文章已隐藏')
-                  break
-                case PostCardOperationType.DeployArticle:
-                  postInfo.isDeploy = true
-                  message.success('文章已发布')
-                  break
-                case PostCardOperationType.UnDeploy:
-                  postInfo.isDeploy = false
-                  message.success('已取消发布')
-                  break
-              }
-            }
-          })
-      }
-    }
+    let postCardOperationHandler =
+        (operationType: PostCardOperationType, postId: string) => {
+          switch (operationType) {
+            case PostCardOperationType.EditPost:
+              isEdit.value = true
+              filename.value = postInfos.value.filter(item => item.id === postId)[0].fileName
+              currentAppWindow.value = WindowName.PostEditor
+              break;
+            case PostCardOperationType.ModifyProperties:
+              showModifyModel.value = true
+              break
+            case PostCardOperationType.BatchOption:
+              // 进入批量编辑前让所有文章处于未选中状态
+              postInfos.value.forEach(postInfo => {
+                if (postInfo.isSelected) {
+                  postInfo.isSelected = false
+                }
+              })
+              break
+            default:
+              api.postApi.postInfoHandle(operationType)!([postId]).then(success => {
+                if (success) {
+                  let postInfo = postInfos.value.filter(item => item.id === postId)[0]
+                  switch (operationType) {
+                    case PostCardOperationType.DeleteArticle:
+                      postInfo.isDeleted = true
+                      message.success('文章已放入回收站')
+                      break
+                    case PostCardOperationType.UnTop:
+                      postInfo.isTop = false
+                      message.success('已取消置顶')
+                      break
+                    case PostCardOperationType.TopArticle:
+                      postInfo.isTop = true
+                      postInfo.isHide = false
+                      message.success('文章已置顶')
+                      break
+                    case PostCardOperationType.UnHide:
+                      postInfo.isHide = false
+                      message.success('已取消隐藏')
+                      break
+                    case PostCardOperationType.HideArticle:
+                      postInfo.isHide = true
+                      postInfo.isTop = false
+                      message.success('文章已隐藏')
+                      break
+                    case PostCardOperationType.DeployArticle:
+                      postInfo.isDeploy = true
+                      message.success('文章已发布')
+                      break
+                    case PostCardOperationType.UnDeploy:
+                      postInfo.isDeploy = false
+                      message.success('已取消发布')
+                      break
+                  }
+                }
+              })
+          }
+        }
     return {
       postInfos,
       showModifyModel,
